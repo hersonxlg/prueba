4c0ecc066e18c4fde8e8deeaa03a4bed222fe6a8
Herson Lozano <hersonx@gmail.com>
2024-06-22 14:08:12 -0600

    delete all

--- a.py
+++
@@ -1,7 +0,0 @@
-
-def saludar():
-    print("hola mundo")
-
-
-if __name__ == '__main__':
-    saludar()
--- list_of_files.py
+++
@@ -1,5 +0,0 @@
-import glob
-
-if __name__ == '__main__':
-    files = glob.glob("*")
-    print(files)
